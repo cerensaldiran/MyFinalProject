e06a6e5797c56213dacf8402e90c0a6e96fb3808
cerensaldiran <cerensaldirann@gmail.com>
2023-02-06 20:56:03 +0300

    added API

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,13 +38,13 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
-            //Yetkisi var mı?
-            if (DateTime.Now.Hour==20)
+            //Yetkisi var mı ?
+            if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-          
-                return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
 
             
         }
