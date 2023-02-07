5e4c6c08b2e3cb96bdbddcebba31f3ea03d890a6
cerensaldiran <cerensaldirann@gmail.com>
2023-02-07 19:28:29 +0300

    added Autofac

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,8 +32,8 @@ namespace WebAPI
         {
             //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
